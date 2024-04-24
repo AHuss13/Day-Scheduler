c07cb3ef62e5ea85d7a7b2d0e18cb7d6248851cd
Adam Huss <adamhuss13@gmail.com>
2024-04-24 12:56:40 -0500

    made boxes for schedule, added color classes, removed old comments, and made new ones

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 $(function () {
-  // Add code to display the current date in the header of the page.
+  // Display date at the top of page
   function displayDate() {
-    let currentDate = dayjs().format('dddd, MMMM D, YYYY');
-    $('#currentDay').text(currentDate)
+    let currentDate = dayjs().format("dddd, MMMM D, YYYY");
+    $("#currentDay").text(currentDate);
   }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Make boxes for schedule
+  function timeBlocks() {
+    let currentTime = dayjs().hour();
 
+    // Loops through hours
+    for (let hour = 9; hour <= 17; hour++) {
+      let modifiedHours;
+      if (hour <= 12) {
+        modifiedHours = hour + "AM";
+      } else {
+        modifiedHours = hour - 12 + "PM"; // fixes hours to display afternoon correctly
+      }
 
+      //Time block elements
+      let $timeBlock = $("<div>").addClass("row time-block");
+      let $hourDiv = $("<div>")
+        .addClass("col-2 col-md-1 hour text-center py-3")
+        .text(modifiedHours);
+      let $inputArea = $("<textarea>")
+        .addClass("col-8 col-md-10 description")
+        .attr("rows", 3);
+      let $saveBtn = $("<button>")
+        .addClass("btn saveBtn col-2 col-md-1")
+        .attr("aria-label", "save")
+        .html('<i class="fas fa-save" aria-hidden="true"></i>');
 
-  let $saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save').html('<i class="fas fa-save" aria-hidden="true"></i>');
+      $timeBlock
+        .attr("id", "hour-" + hour)
+        .append($hourDiv, $inputArea, $saveBtn);
+      $(".container-fluid").append($timeBlock);
 
+      // Add classes to color boxes based on time
+      if (hour < currentTime) {
+        $timeBlock.addClass("past");
+      } else if (hour === currentTime) {
+        $timeBlock.addClass("present");
+      } else {
+        $timeBlock.addClass("future");
+      }
+    }
+  }
+  
+  // Save button functionality
+  $(".container-fluid").on("click", ".saveBtn", function () {
+    let eventId = $(this).closest(".time-block").attr("id");
+    let eventDescription = $(this).siblings(".description").val();
+    localStorage.setItem(eventId, eventDescription);
+  });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // Get saved schedule info
   function getEvents() {
-    $('.time-block').each(function () {
-      let eventId = $(this).attr('id');
-      var savedEvent = localStorage.getItem(eventId);
+    $(".time-block").each(function () {
+      let eventId = $(this).attr("id");
+      let savedEvent = localStorage.getItem(eventId);
 
       if (savedEvent) {
-        $(this).find('.description').val(savedEvent);
+        $(this).find(".description").val(savedEvent);
       }
-    })
+    });
   }
-
 
   // Functions...ASSEMBLE! 🛡️🏹
   displayDate();
